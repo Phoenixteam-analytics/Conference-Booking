7ba2acd6f0c256fd2a01e918cf4fe199772bf9fc
Phoenixteam-analytics <kteja@phoenixteam.com>
2024-11-21 20:14:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -172,7 +172,7 @@ if page == "Book a Conference Room":
             user_name = st.text_input("Your Name", placeholder="Enter your full name")
             user_email = st.text_input("Your Email", placeholder="Enter your email")
         with col2:
-            selected_room = st.selectbox("Choose Room", [""Collaborate", "Innovate", "Echo","Vibe"])
+            selected_room = st.selectbox("Choose Room", ["Collaborate", "Innovate", "Echo","Vibe"])
         with col3:
             priority = st.selectbox("Priority Level", ["Low", "Medium-Low", "Medium", "Medium-High", "High"])
 
